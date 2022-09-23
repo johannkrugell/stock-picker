87d4a9bf991c69326a32d1c046ae94d186731bc4
johannkrugell <johannkrugell@gmail.com>
2022-09-24 09:27:47 +1000

    ADDED INDEX FOR BUY AND SELL DATE Added the recording of the index of the buy price and the index of the sell price. If the buy index is less than the sell index, record the buy index and sell index in an array. I also added the profit calculation to the array. Sell price at the index less the buy price at the index. The each array, buy / sell array and profit array, is appended to an array to collect all profits at various buy and sell indexes
    On branch add-index-day
    Changes to be committed:
    modified:   stock_picker.rb

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,30 +1,23 @@
 # stock-price data
-stock_prices = [17,3,6]
+stock_prices = [17,3,2]
 
 # stock picker method
 def stock_picker(stock_prices)
-  
-  index_to_remove = []
   buy_prices = stock_prices
   profit = []
+  
   stock_prices.each_with_index do |sell_price, sell_index| 
     buy_prices_filter = []
-      p "Sell price $#{sell_price} + index: #{sell_index}"
-    buy_prices.each_with_index do |buy_price, buy_index|
-      
-      p "Buy price $#{buy_price} + index: #{buy_index}"
-      
+    p "Sell price $#{sell_price} + index: #{sell_index}"
+  
+    buy_prices.each_with_index do |buy_price, buy_index| 
       if buy_index < sell_index
-      
-        buy_prices_filter << buy_price
+        p "Buy price $#{buy_price} + index: #{buy_index}"
+        p buy_prices_filter << ["#{buy_index}:#{sell_index}",sell_price - buy_price]
       end
-      
     end
-    p buy_prices_filter
-      buy_prices_filter.each do |price| 
-        profit << sell_price - price
-      end
-    p profit
+     profit << buy_prices_filter
+     p profit
   end
 end
 
