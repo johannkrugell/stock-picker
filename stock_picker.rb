1e08bfae45d0526c96a57f8ddb7a6fb972b248de
johannkrugell <johannkrugell@gmail.com>
2022-09-24 10:06:04 +1000

    REMOVE EMPTY ARRAY, CREATE HASH, FILTER MAX VALUE The program is now a working version of the assigment. I added the a filter to remove any empty arrays, cleared the array after each new day/sell/profit was added. Convert the array to a hash and use max_by to return the maximum value. Code should now be refactored.
    On branch remove-empty-array
    Changes to be committed:
    modified:   stock_picker.rb

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,5 +1,5 @@
 # stock-price data
-stock_prices = [17,3,2]
+stock_prices = [17,3,6,9,15,8,6,1,10]
 
 # stock picker method
 def stock_picker(stock_prices)
@@ -8,17 +8,22 @@ def stock_picker(stock_prices)
   
   stock_prices.each_with_index do |sell_price, sell_index| 
     buy_prices_filter = []
-    p "Sell price $#{sell_price} + index: #{sell_index}"
+    #p "Sell price $#{sell_price} + index: #{sell_index}"
   
     buy_prices.each_with_index do |buy_price, buy_index| 
       if buy_index < sell_index
-        p "Buy price $#{buy_price} + index: #{buy_index}"
-        p buy_prices_filter << ["#{buy_index}:#{sell_index}",sell_price - buy_price]
-      end
+        #p "Buy price $#{buy_price} + index: #{buy_index}"
+        buy_prices_filter << ["#{buy_index}:#{sell_index}",sell_price - buy_price]
+        profit << buy_prices_filter.to_h
+        buy_prices_filter.clear    
+        end
     end
-     profit << buy_prices_filter
-     p profit
+     # profit << buy_prices_filter.to_h
+     profit.flatten
   end
+  hash = profit.reject { |day| day.empty? }
+  hash =  hash.reduce Hash.new, :merge
+  p hash.max_by { |day, profit| profit }
 end
 
 stock_picker(stock_prices)
