bd3f6a3a3fcd254f0512139ccee8929bb4307025
johannkrugell <johannkrugell@gmail.com>
2022-09-25 05:55:00 +1000

    REFACTOR Refactored the declaration of a seperate buy_price array and the if statement with a select of the sell price array. Conversion to a hash and finding the max element in the hash was chained into one statement
    On branch refactor
    Changes to be committed:
    modified:   stock_picker.rb

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,29 +1,20 @@
 # stock-price data
 stock_prices = [17,3,6,9,15,8,6,1,10]
 
-# stock picker method
+# method returns the index of the buy price, sell price and max profit
 def stock_picker(stock_prices)
-  buy_prices = stock_prices
   profit = []
   
+  # for each sell price find the buy price, calc profit 
   stock_prices.each_with_index do |sell_price, sell_index| 
-    buy_prices_filter = []
-    #p "Sell price $#{sell_price} + index: #{sell_index}"
-  
-    buy_prices.each_with_index do |buy_price, buy_index| 
-      if buy_index < sell_index
-        #p "Buy price $#{buy_price} + index: #{buy_index}"
-        buy_prices_filter << ["#{buy_index}:#{sell_index}",sell_price - buy_price]
-        profit << buy_prices_filter.to_h
-        buy_prices_filter.clear    
-        end
-    end
-     # profit << buy_prices_filter.to_h
-     profit.flatten
+    stock_prices.each_with_index.select { |buy_price, buy_index| 
+      if buy_index < sell_index 
+        profit << ["#{buy_index}:#{sell_index}",sell_price - buy_price]
+      end
+    }
   end
-  hash = profit.reject { |day| day.empty? }
-  hash =  hash.reduce Hash.new, :merge
-  p hash.max_by { |day, profit| profit }
+  
+  profit.to_h.max_by { |day, profit| profit }
 end
 
-stock_picker(stock_prices)
+p stock_picker(stock_prices)
